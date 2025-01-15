b4af07357952970b1690af40865eed8766e87033
Julia_Sheveleva <julia_sheveleva@senla.eu>
2025-01-15 11:51:40 +0300

    Lesson 18

--- src/main/java/com/itgirls/socialMedia/repository/UserRepository.java
+++ src/main/java/com/itgirls/socialMedia/repository/UserRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User getUserById(Long id);
+    User getUserById(Long ids);
 
     List<User> getAllByNameOrderByIdDesc(String name);
 
